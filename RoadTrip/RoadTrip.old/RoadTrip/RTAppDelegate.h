0eff0283bd442132e8643fd7436f2222b96ec554
Frédéric Occédat <fred@MacBook-Pro-Fred.local>
2012-07-26 19:02:52 +0100

    New project RoadTrip

---
+++ RoadTrip/RoadTrip.old/RoadTrip/RTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RTAppDelegate.h
+//  RoadTrip
+//
+//  Created by Frédéric Occédat on 24/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RoadTrip/RoadTrip.old/RoadTrip/RTViewController.h
@@ -0,0 +1,13 @@
+//
+//  RTViewController.h
+//  RoadTrip
+//
+//  Created by Frédéric Occédat on 24/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTViewController : UIViewController
+
+@end
